2b722eda7e9d39561f73c0496a671eabe775c298
Ernesto Fuentes <ernesto_fuentes12@tamu.edu>
2024-02-01 09:16:18 -0600

    Updated Data to database.py

--- Data to database.py
+++ Data to database.py
@@ -1,14 +1,27 @@
+"""
+ECEN 404 - 901
+Group # 50
+Ernesto Fuentes
+Code derived with help from Nicholas Tran
+Subsystem - Data Scrapping
+"""
+#####################   
+# Imports required
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 import pypyodbc
 import pandas as pd
 
+###################################
 
+# declares the variables and defines them for the server connections, along with the table names that are going to be assigned
 SERVER_NAME = 'tcp:tecafs.database.windows.net,1433'
 DATABASE_NAME = 'TecafsSqlDatabase'
-TABLE_NAME = 'clean_data1'
+TABLE_NAME = 'clean_data'
 
+###################################
 
+# makes the connection to the database with the connection string; has the driver, server name, database name, id, and password
 connection_string = f"""
     DRIVER={{ODBC Driver 18 for SQL Server}};
     SERVER={SERVER_NAME};
@@ -16,8 +29,13 @@ connection_string = f"""
     Uid={'tecafs2023'};
     Pwd={'Capstone50'};
 """
-connection_string1 = pypyodbc.connect("Driver={ODBC Driver 18 for SQL Server};Server=tcp:tecafs.database.windows.net,1433;Database=TecafsSqlDatabase;Uid=tecafs2023;Pwd={Capstone50};")
+###################################
 
+# attempted connection string, didn't work though
+
+#connection_string1 = pypyodbc.connect("Driver={ODBC Driver 18 for SQL Server};Server=tcp:tecafs.database.windows.net,1433;Database=TecafsSqlDatabase;Uid=tecafs2023;Pwd={Capstone50};")
+
+###################################
 
 
 connection_url = URL.create('mssql+pyodbc', query={'odbc_connect': connection_string})
@@ -30,31 +48,4 @@ for sheet_name, df_data in excel_file.items():
     #{'fail', 'replace', 'append'}
     df_data.to_sql(TABLE_NAME, engine, if_exists='append', index=False)
 
-
-#SERVER_NAME = 'tcp:tecafs.database.windows.net,1433'
-#DATABASE_NAME = 'TecafsSqlDatabase'
-#TABLE_NAME = 'weather_data'
-
-
-#connection_string = f"""
- #   DRIVER={{ODBC Driver 18 for SQL Server}};
-  #  SERVER={SERVER_NAME};
-   # DATABASE={DATABASE_NAME};
-    #Uid={'tecafs2023'};
-    #Pwd={'Capstone50'};
-#"""
-#connection_string1 = pypyodbc.connect("Driver={ODBC Driver 18 for SQL Server};Server=tcp:tecafs.database.windows.net,1433;Database=TecafsSqlDatabase;Uid=tecafs2023;Pwd={Capstone50};")
-
-
-
-#connection_url = URL.create('mssql+pyodbc', query={'odbc_connect': connection_string})
-#engine = create_engine(connection_url, module=pypyodbc)
-
-
-#excel_file = pd.read_csv(r"C:\Users\netof\OneDrive\Desktop\capstone\output.csv")
-#for sheet_name, df_data in excel_file.items():
- #   print(f'Loading worksheet {sheet_name}...')
-  #  {'fail', 'replace', 'append'}
-   # df_data.to_sql(TABLE_NAME, engine, if_exists='append', index=False)
-
-
+###################################
